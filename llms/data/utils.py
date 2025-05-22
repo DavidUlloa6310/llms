Fix GPT-2 exploding gradients on training, improve logging, and add checkpointing

llms/data/utils.py
@@ -1,16 +1,8 @@
 import pickle
 
+import numpy as np
 import jax
 import jax.numpy as jnp
-
-def save_dataset(dataset, path: str, filename: str):
-    with open(f"{path}/{filename}", "wb") as f:
-        pickle.dump(dataset, f)
-
-
-def read_dataset(path: str):
-    with open(path, "rb") as f:
-        return pickle.load(f)
 
 
 def batch_dataset(dataset, batch_size):
@@ -18,8 +10,11 @@ def batch_dataset(dataset, batch_size):
     for example in dataset:
         batch["input_ids"].append(example["input_ids"])
         if len(batch["input_ids"]) == batch_size:
-            yield {k: jnp.array(v, dtype=jnp.int32) for k, v in batch.items()}
+            yield {k: np.array(v, dtype=np.int32) for k, v in batch.items()}
             batch = {"input_ids": []}
+
+    if batch["input_ids"]:
+        yield {k: np.array(v, dtype=np.int32) for k, v in batch.items()}
 
 
 def shard_batch(batch):

llms/inference.py
@@ -5,6 +5,7 @@ import jax
 import jax.numpy as jnp
 from transformers import AutoTokenizer
 import optax
+from flax.training import checkpoints
 from flax.training.train_state import TrainState
 import flax.jax_utils as flax_utils
 
@@ -14,14 +15,17 @@ from llms.models.gpt2 import GPT2, GPT2Config
 tokenizer = AutoTokenizer.from_pretrained("gpt2")
 tokenizer.pad_token = tokenizer.eos_token
 
+
 def unshard_params(state):
-    unsharded_params = jax.tree.map(lambda x: x[0] if x.shape[0] == 8 else x, state.params)
+    unsharded_params = jax.tree.map(
+        lambda x: x[0] if x.shape[0] == 8 else x, state.params)
     unsharded_state = TrainState.create(
         apply_fn=model.apply,
         params=unsharded_params,
         tx=optax.adamw(learning_rate=5e-5)
     )
     return unsharded_params, unsharded_state
+
 
 def generate(
     state,
@@ -31,48 +35,25 @@ def generate(
     num_tokens_to_generate=50,
     temperature=1.0,
 ):
-    # logits, key_cache, value_cache = state.apply_fn(
-    #     {"params": state.params},
-    #     input_ids,
-    #     # key_cache = None,
-    #     # value_cache = None,
-    #     deterministic=True
-    # )
-    logits = state.apply_fn(
-        {"params": state.params},
-        input_ids,
-        # key_cache = key_cache,
-        # value_cache = value_cache,
-        deterministic=True
-    )
-    generated_ids = input_ids
-    for i in range(num_tokens_to_generate):
-        last_logits = logits[:, -1, :] / temperature
-        
-        rng, subrng = jax.random.split(rng)
-        next_token_id = jax.random.categorical(subrng, last_logits)  # shape: (batch,)
-        
-        next_token_id = next_token_id[:, None]  # (batch, 1)
-        generated_ids = jnp.concatenate([generated_ids, next_token_id], axis=1)
-        
-        # logits_new, key_cache, value_cache = state.apply_fn(
-        #     {"params": state.params},
-        #     next_token_id,
-        #     key_cache = key_cache,
-        #     value_cache = value_cache,
-        #     deterministic=True
-        # )
-        logits_new= state.apply_fn(
-            {"params": state.params},
-            next_token_id,
-            # key_cache = key_cache,
-            # value_cache = value_cache,
-            deterministic=True
-        )
-        # logits_new shape: (batch, 1, vocab_size)
-        logits = jnp.concatenate([logits, logits_new], axis=1)
+    generated = input_ids  # shape: (batch, cur_len)
 
-    return generated_ids
+    for _ in range(num_tokens_to_generate):
+        # run the model on everything so far
+        logits = state.apply_fn(
+            {"params": state.params},
+            generated,
+            deterministic=True,
+        )  # (batch, cur_len, vocab_size)
+
+        # sample from the last position
+        last_logits = logits[:, -1, :] / temperature
+        rng, subrng = jax.random.split(rng)
+        next_token = jax.random.categorical(subrng, last_logits)
+        next_token = next_token[:, None]  # (batch,1)
+
+        generated = jnp.concatenate([generated, next_token], axis=1)
+    return generated
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -84,37 +65,41 @@ if __name__ == '__main__':
     # ===================
     # Defining Model
     # ===================
-    config = GPT2Config()
-    model = GPT2(config)
+    model = GPT2(GPT2Config())
 
     rng = jax.random.PRNGKey(0)
     dummy_input = jnp.zeros((1, 1024), dtype=jnp.int32)
     params = model.init(rng, dummy_input)['params']
 
     optimizer = optax.adamw(learning_rate=5e-5)
-    state = TrainState.create(apply_fn=model.apply, params=params, tx=optimizer)
+    state = TrainState.create(apply_fn=model.apply,
+                              params=params, tx=optimizer)
 
     print("Available Devices:", jax.device_count())
     param_count = sum(p.size for p in jax.tree_util.tree_leaves(params))
     print("Total Parameters:", param_count, f"({param_count / 1e6:.2f}M)")
 
-    state = load_model(state, args.path)
+    state = checkpoints.restore_checkpoint(
+        ckpt_dir=str(args.path),
+        target=state,
+        prefix="gpt_2_checkpoint_"
+    )
 
-    input_text = "Once upon a time, Urne, a father"
-    encoded_input = tokenizer.encode(input_text, return_tensors="np")  # shape: (1, prefix_length)
+    input_text = "Once upon a time"
+    encoded_input = tokenizer.encode(
+        input_text, return_tensors="np")  # shape: (1, prefix_length)
     input_ids = jnp.array(encoded_input)
 
     rng = jax.random.PRNGKey(0)
     num_tokens = 50
     temperature = 0.7
 
-    _, unshareded_state = unshard_params(state)
+    state = flax_utils.unreplicate(state)
 
     final_ids = generate(
-        state = unshareded_state,
+        state=state,
         input_ids=input_ids,
         rng=rng,
-        config=config,
         num_tokens_to_generate=num_tokens,
         temperature=temperature,
     )

llms/models/gpt2.py
@@ -1,7 +1,21 @@
 from dataclasses import dataclass
 
 import flax.linen as nn
+import jax
 import jax.numpy as jnp
+
+
+def assert_finite(tag: str, x: jax.Array):
+    bad = ~jnp.isfinite(x)
+
+    def _raise(tensor):
+        jax.debug.print("{tag}: non-finite detected", tag=tag)
+        # you can break or throw after the print; returning `tensor`
+        # keeps the pytree structure identical
+        return tensor
+
+    # both branches now return `x` (the same array shape/dtype)
+    _ = jax.lax.cond(jnp.any(bad), _raise, lambda tensor: tensor, operand=x)
 
 
 @dataclass
@@ -15,7 +29,9 @@ class GPT2Config:
     layer_norm_epsilon: float = 1e-5
     dropout_rate: float = 0.1
     attn_dropout_rate: float = 0.1
-    initializer_range: float = 0.02
+    initializer_range: float = 0.01
+    dtype: jnp.dtype = jnp.bfloat16  # dtype used in computations
+    compute_dtype: jnp.dtype = jnp.float32  # dtype used to store parameters
 
 
 class MHSelfAttention(nn.Module):
@@ -34,7 +50,9 @@ class MHSelfAttention(nn.Module):
             cfg.n_embd * 3,
             use_bias=True,
             kernel_init=nn.initializers.normal(cfg.initializer_range),
-            bias_init=nn.initializers.zeros
+            bias_init=nn.initializers.zeros,
+            dtype=cfg.dtype,
+            param_dtype=cfg.param_dtype
         )(hidden_states)
 
         qkv = qkv.reshape(batch_size, seq_length, 3,
@@ -49,15 +67,19 @@ class MHSelfAttention(nn.Module):
         k = k.transpose((0, 2, 1, 3))
         v = v.transpose((0, 2, 1, 3))
 
-        causal_mask = jnp.tril(jnp.ones((seq_length, seq_length)))
+        causal_mask = jnp.tril(
+            jnp.ones((seq_length, seq_length), dtype=cfg.dtype))
         causal_mask = causal_mask.reshape(1, 1, seq_length, seq_length)
 
         # Scaled dot-product attention
-        dk = jnp.sqrt(k.shape[-1]).astype(q.dtype)
-        attn_weights = jnp.matmul(q, k.transpose((0, 1, 3, 2))) / dk
+        inv_sqrt_d = jnp.asarray(
+            1.0 / jnp.sqrt(k.shape[-1]), dtype=cfg.dtype)
+
+        attn_weights = jnp.matmul(q, k.transpose(0, 1, 3, 2)) * inv_sqrt_d
 
         # Apply the mask
-        attn_weights = jnp.where(causal_mask == 0, -1e10, attn_weights)
+        neg_inf = jnp.full_like(attn_weights, -1e4, dtype=cfg.dtype)
+        attn_weights = jnp.where(causal_mask == 0, neg_inf, attn_weights)
 
         attn_probs = nn.softmax(attn_weights, axis=-1)
         attn_probs = nn.Dropout(rate=cfg.attn_dropout_rate)(
@@ -77,7 +99,9 @@ class MHSelfAttention(nn.Module):
             cfg.n_embd,
             use_bias=True,
             kernel_init=nn.initializers.normal(cfg.initializer_range),
-            bias_init=nn.initializers.zeros
+            bias_init=nn.initializers.zeros,
+            param_dtype=cfg.param_dtype,
+            dtype=cfg.dtype,
         )(attn_output)
 
         return out
@@ -90,17 +114,24 @@ class MLP(nn.Module):
     def __call__(self, hidden_states, deterministic: bool = True):
         cfg = self.config
 
+        hidden_states = hidden_states.astype(cfg.dtype)
+
         hidden = nn.Dense(
             cfg.n_inner,
             kernel_init=nn.initializers.normal(cfg.initializer_range),
-            bias_init=nn.initializers.zeros
+            bias_init=nn.initializers.zeros,
+            dtype=cfg.dtype,
+            param_dtype=cfg.param_dtype
         )(hidden_states)
+        hidden = jnp.clip(hidden, -10, 10)
         hidden = nn.gelu(hidden)
 
         hidden = nn.Dense(
             cfg.n_embd,
             kernel_init=nn.initializers.normal(cfg.initializer_range),
-            bias_init=nn.initializers.zeros
+            bias_init=nn.initializers.zeros,
+            dtype=cfg.dtype,
+            param_dtype=cfg.param_dtype
         )(hidden)
 
         hidden = nn.Dropout(rate=cfg.dropout_rate)(
@@ -115,14 +146,24 @@ class TransformerBlock(nn.Module):
     def __call__(self, hidden_states, deterministic: bool = True):
         cfg = self.config
 
-        attn_ln = nn.LayerNorm(epsilon=cfg.layer_norm_epsilon)(hidden_states)
+        hidden_states = hidden_states.astype(cfg.dtype)
+
+        attn_ln = nn.LayerNorm(
+            epsilon=cfg.layer_norm_epsilon,
+            dtype=cfg.dtype,
+            param_dtype=cfg.param_dtype
+        )(hidden_states)
         attn_out = MHSelfAttention(cfg)(attn_ln, deterministic=deterministic)
         attn_out = nn.Dropout(rate=cfg.dropout_rate)(
             attn_out, deterministic=deterministic)
 
         x = hidden_states + attn_out
 
-        mlp_ln = nn.LayerNorm(epsilon=cfg.layer_norm_epsilon)(x)
+        mlp_ln = nn.LayerNorm(
+            epsilon=cfg.layer_norm_epsilon,
+            param_dtype=cfg.param_dtype,
+            dtype=cfg.dtype
+        )(x)
         mlp_out = MLP(cfg)(mlp_ln, deterministic=deterministic)
 
         # Residual
@@ -140,23 +181,30 @@ class GPT2(nn.Module):
             num_embeddings=self.config.vocab_size,
             features=self.config.n_embd,
             embedding_init=nn.initializers.normal(
-                self.config.initializer_range)
+                self.config.initializer_range),
+            param_dtype=self.config.param_dtype,
+            dtype=self.config.dtype
         )
         self.wpe = self.param(
             "wpe",
             nn.initializers.normal(self.config.initializer_range),
-            (self.config.max_position_embeddings, self.config.n_embd)
+            (self.config.max_position_embeddings, self.config.n_embd),
+            dtype=self.config.param_dtype,
         )
-        self.blocks = [TransformerBlock(self.config)
-                       for _ in range(self.config.n_layer)]
-        self.ln_f = nn.LayerNorm(epsilon=self.config.layer_norm_epsilon)
+        self.blocks = [TransformerBlock(self.config, name=f"transformer_block_{i}")
+                       for i in range(self.config.n_layer)]
+        self.ln_f = nn.LayerNorm(
+            epsilon=self.config.layer_norm_epsilon,
+            param_dtype=self.config.param_dtype,
+            dtype=self.config.dtype
+        )
         self.dropout = nn.Dropout(rate=self.config.dropout_rate)
 
     def __call__(self, input_ids, deterministic=True):
         # Input embedding
         position_ids = jnp.arange(input_ids.shape[1])[None, :]
         token_embeds = self.wte(input_ids)
-        position_embeds = self.wpe[position_ids, :]
+        position_embeds = self.wpe[position_ids, :].astype(self.config.dtype)
         hidden_states = token_embeds + position_embeds
         hidden_states = self.dropout(
             hidden_states, deterministic=deterministic)
@@ -170,7 +218,8 @@ class GPT2(nn.Module):
 
         # Output logits by tying embeddings
         # We transpose the embedding matrix
-        # from [vocab_size, n_embd] to [n_embd, vocab_size]
-        wte_tied = self.wte.embedding.T  # shape: [n_embd, vocab_size]
+        wte_tied = self.wte.embedding.T.astype(
+            self.config.dtype
+        )  # from [vocab_size, n_embd] to [n_embd, vocab_size]
         logits = jnp.einsum("bld,dk->blk", hidden_states, wte_tied)
         return logits

llms/train.py
@@ -7,31 +7,35 @@ import jax
 import jax.numpy as jnp
 import flax.linen as nn
 import flax.jax_utils as flax_utils
+from flax.training import checkpoints
 from flax.training.train_state import TrainState
 import optax
 from tqdm import tqdm
 import wandb
+from datasets import load_from_disk
 
 from llms.models.gpt2 import GPT2Config, GPT2
-from llms.models.utils import save_model
-from llms.data.openweb import download_openweb
-from llms.data.utils import shard_batch, batch_dataset, read_dataset
+from llms.data.openweb import build_openweb
+from llms.data.utils import shard_batch, batch_dataset
 
 
 @partial(jax.pmap,
-         axis_name="batch",
-         static_broadcasted_argnums=(3,))
-def train_step(state, batch, dropout_rngs, max_grad_norm):
+         axis_name="batch", static_broadcasted_argnums=(3,))
+def train_step(state, batch, rng, clip_norm):
+    dropout_key = jax.random.fold_in(rng, jax.lax.axis_index('batch'))
+
     def loss_fn(params):
         logits = state.apply_fn(
             {'params': params},
             batch['input_ids'],
             deterministic=False,
-            rngs={"dropout": dropout_rngs}
+            rngs={"dropout": dropout_key}
         )
+        # jax.debug.print("logits in loss_fn: {x}", x=logits)
         loss = jnp.mean(optax.softmax_cross_entropy_with_integer_labels(
             logits[:, :-1, :], batch['input_ids'][:, 1:]
         ))
+
         return loss
 
     loss, grads = jax.value_and_grad(loss_fn)(state.params)
@@ -39,12 +43,42 @@ def train_step(state, batch, dropout_rngs, max_grad_norm):
     averaged_grads = jax.lax.pmean(grads, axis_name='batch')
     averaged_loss = jax.lax.pmean(loss, axis_name='batch')
 
-    clipper = optax.clip_by_global_norm(max_grad_norm)
-    clipped_grads, _ = clipper.update(averaged_grads, optax.EmptyState(), None)
+    raw_norm = optax.global_norm(averaged_grads)
 
-    new_state = state.apply_gradients(grads=clipped_grads)
-    grad_norm = optax.global_norm(averaged_grads)
-    return new_state, averaged_loss, grad_norm
+    # emulate what Adam receives after grad clipping
+    clipper = optax.clip_by_global_norm(clip_norm)
+    clipped_grads, _ = clipper.update(averaged_grads, optax.EmptyState(), None)
+    used_norm = optax.global_norm(clipped_grads)
+
+    new_state = state.apply_gradients(grads=averaged_grads)
+
+    metrics = {
+        "train_loss": averaged_loss,
+        "grad_norm": raw_norm,
+        "clipped_grad_norm": used_norm
+    }
+
+    return new_state, metrics
+
+
+def build_scheduler(total_steps, config):
+    warmup_schedule = optax.linear_schedule(
+        init_value=0.0,
+        end_value=config.peak_lr,
+        transition_steps=config.warmup_steps,
+    )
+    plateau_schedule = optax.constant_schedule(value=config.peak_lr)
+    decay_steps = total_steps - config.warmup_steps - config.plateau_steps
+    decay_schedule = optax.cosine_decay_schedule(
+        init_value=config.peak_lr,
+        decay_steps=decay_steps,
+        alpha=(config.peak_lr * config.end_lr_factor) / config.peak_lr,
+    )
+    return optax.join_schedules(
+        schedules=[warmup_schedule, plateau_schedule, decay_schedule],
+        boundaries=[config.warmup_steps,
+                    config.warmup_steps + config.plateau_steps],
+    )
 
 
 def main():
@@ -52,99 +86,141 @@ def main():
         description="JAX/Flax GPT-2 pre-training script")
     parser.add_argument("--checkpoint-path", type=Path, required=True,
                         help="Directory to store msgpack checkpoints")
-    parser.add_argument("--dataset-path", type=Path,
+    parser.add_argument("--resume", action="store_true", required=False,
+                        help="Resume training from latest checkpoint inside --resume-path folder")
+    parser.add_argument("--dataset-path", type=Path, required=False,
                         help="OpenWebText dataset path (downloaded if absent)")
+    parser.add_argument("--num-chkpts", type=int, default=10,
+                        help="Number of checkpoints to save during training")
     args = parser.parse_args()
     args.checkpoint_path.mkdir(parents=True, exist_ok=True)
 
-    dataset = (read_dataset(args.dataset_path) if args.dataset_path
-               else download_openweb())["train"]
+    dataset = load_from_disk(
+        args.dataset_path) if args.dataset_path else build_openweb()
 
-    config = GPT2Config()
-    model = GPT2(config)
+    model = GPT2(GPT2Config())
 
     wandb.login()
-    run = wandb.init(
+    wandb.init(
         project="gpt-2-pretraining",
         config=dict(
             epochs=1,
             seq_len=1024,
-            batch_size=jax.device_count(),
-            accum_steps=8,
+            batch_size=8 * jax.device_count(),
+            accum_steps=4,
             max_norm=2.0,
-            peak_lr=1e-6,
+            peak_lr=5e-5,
             warmup_steps=2_000,
             adam_eps=1e-8,
             adam_b1=0.9,
             adam_b2=0.95,
             weight_decay=0.01,
             end_lr_factor=0.1,
+            plateau_steps=2_000,
         ),
     )
+    wandb.define_metric("examples_seen")
+    wandb.define_metric("train_loss", step_metric="examples_seen")
+    wandb.define_metric("perplexity", step_metric="examples_seen")
+    wandb.define_metric("grad_norm", step_metric="examples_seen")
+    wandb.define_metric("clipped_grad_norm", step_metric="examples_seen")
+    wandb.define_metric("learning_rate")
     config = wandb.config
 
-    steps_per_epoch = math.ceil(len(dataset) / jax.device_count())
+    steps_per_epoch = math.ceil(len(dataset) / config.batch_size)
     total_steps = config.epochs * steps_per_epoch
-    lr_schedule = optax.warmup_cosine_decay_schedule(
-        init_value=0.0,
-        peak_value=config.peak_lr,
-        warmup_steps=config.warmup_steps,
-        decay_steps=total_steps - config.warmup_steps,
-        end_value=config.peak_lr * config.end_lr_factor,
-    )
-    tx = optax.adamw(
-        learning_rate=lr_schedule,
-        b1=config.adam_b1,
-        b2=config.adam_b2,
-        eps=config.adam_eps,
-        weight_decay=config.weight_decay,
+    lr_schedule = build_scheduler(total_steps, config)
+
+    tx = optax.chain(
+        optax.clip_by_global_norm(config.max_norm),
+        optax.MultiSteps(
+            optax.adamw(
+                learning_rate=lr_schedule,
+                b1=config.adam_b1,
+                b2=config.adam_b2,
+                eps=config.adam_eps,
+                weight_decay=config.weight_decay,
+            ),
+            every_k_schedule=config.accum_steps,
+        )
     )
 
     rng = jax.random.PRNGKey(0)
     init_rng, training_rng = jax.random.split(rng)
+
     dummy_in = jnp.zeros((1, config.seq_len), dtype=jnp.int32)
     params = model.init(init_rng, dummy_in)["params"]
-
     state = TrainState.create(apply_fn=model.apply, params=params, tx=tx)
+
+    if args.resume:
+        state = checkpoints.restore_checkpoint(
+            ckpt_dir=str(args.checkpoint_path),
+            target=state,
+            prefix="gpt_2_checkpoint_"
+        )
+        print("Restored model from checkpoint, step:", state.step)
+
     state = flax_utils.replicate(state)
 
-    param_cnt = sum(p.size for p in jax.tree_util.tree_leaves(params))
     print(f"Available devices : {jax.device_count()}")
+    param_cnt = sum(p.size for p in jax.tree_util.tree_leaves(params))
     print(f"Total parameters  : {param_cnt:,} ({param_cnt/1e6:.2f} M)")
 
+    print("Model dtype: ", state.params['blocks_0']
+          ['MHSelfAttention_0']['Dense_0']['kernel'].dtype)
+
+    # for checkpoint saving
+    save_interval = max(1, total_steps // args.num_chkpts)
+    print(f"Total Steps: {total_steps}, saving every {save_interval} steps")
+
     for epoch in range(1, config.epochs + 1):
-        step = 0
         progress_bar = tqdm(desc=f"Epoch {epoch}", unit="batch")
 
-        for batch in batch_dataset(dataset, batch_size=config.batch_size):
-            batch = shard_batch(batch)
+        batches = (shard_batch(b) for b in batch_dataset(
+            dataset, batch_size=config.batch_size))
+        # Load only 3 batches into memory ahead of time (good for GPUs)
+        prefetch_iter = flax_utils.prefetch_to_device(batches, size=3)
 
-            curr_step_rng, training_rng = jax.random.split(training_rng)
+        for batch in prefetch_iter:
             # curr_step_rng split uniquely for each device
-            dropout_rngs = jax.random.split(curr_step_rng, jax.device_count())
+            curr_step_rng, training_rng = jax.random.split(training_rng)
+            device_rngs = jax.random.split(curr_step_rng, jax.device_count())
 
-            state, loss, grad_norm = train_step(
-                state, batch, dropout_rngs, config.max_norm)
+            state, metrics = train_step(
+                state, batch, device_rngs, config.max_norm)
 
-            if step % 100 == 0:
-                progress_bar.set_postfix(loss=loss[0])
-            step += 1
+            update_step = int(flax_utils.unreplicate(state).step)
+            if (update_step > 0 and update_step % save_interval == 0) or (update_step == total_steps):
+                chkpt_state = flax_utils.unreplicate(state)
+                checkpoints.save_checkpoint(
+                    args.checkpoint_path,
+                    target=chkpt_state,
+                    step=update_step,
+                    overwrite=False,
+                    keep=args.num_chkpts,
+                    prefix="gpt_2_checkpoint_"
+                )
+                print(f"Saved model checkpoint at step {update_step}")
+
+            if update_step % 100 == 0:
+                progress_bar.set_postfix(loss=metrics['train_loss'][0])
             progress_bar.update(1)
 
             wandb.log({
-                "train_loss": loss[0],
-                "grad_norm": grad_norm[0],
-                "learning_rate": lr_schedule(step),
-            }, step)
+                "examples_seen": update_step * config.batch_size * config.accum_steps,
+                "train_loss": metrics['train_loss'][0],
+                "perplexity": math.exp(metrics['train_loss'][0]),
+                "grad_norm": metrics['grad_norm'][0],
+                "clipped_grad_norm": metrics['clipped_grad_norm'][0],
+                "learning_rate": lr_schedule(update_step),
+            }, step=update_step, commit=update_step % 100 == 0)  # only commit every 100 steps
 
-        save_model(state, f"checkpoint_epoch_{epoch}.msgpack")
-        print(f"Epoch {epoch} completed. Loss: {loss.mean()}")
+        print(f"Epoch {epoch} completed. Loss: {metrics['train_loss'][0]}")
 
     progress_bar.close()
     wandb.finish()
 
 
 if __name__ == "__main__":
-    jax.config.update("jax_debug_nans", True)
-    jax.config.update("jax_debug_infs", True)
+    jax.config.update("jax_default_matmul_precision", "highest")
     main()